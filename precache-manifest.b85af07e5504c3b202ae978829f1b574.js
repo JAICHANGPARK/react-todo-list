f43ed718d1f848f0a093e477efa099cc21d59e07
Dreamwalker <aristojeff@gmail.com>
2019-03-23 20:40:21 +0900

    Updates

---
+++ precache-manifest.b85af07e5504c3b202ae978829f1b574.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9191f166899364c4ca57",
+    "url": "/react-todo-list/static/css/main.5854523b.chunk.css"
+  },
+  {
+    "revision": "9191f166899364c4ca57",
+    "url": "/react-todo-list/static/js/main.5ee01682.chunk.js"
+  },
+  {
+    "revision": "14e46cb4c53468dced63",
+    "url": "/react-todo-list/static/js/runtime~main.d51d0b1e.js"
+  },
+  {
+    "revision": "bd014ca174a8b09a6d53",
+    "url": "/react-todo-list/static/js/2.a4bbbf98.chunk.js"
+  },
+  {
+    "revision": "ad6e7c6486e18cc07af94b33803cb827",
+    "url": "/react-todo-list/index.html"
+  }
+];
